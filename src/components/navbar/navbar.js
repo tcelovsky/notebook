0a961e3a9d83df684c23c2200abe7b371b2ed9d6
Tatyana Celovsky <33358502+tcelovsky@users.noreply.github.com>
2022-11-08 16:44:27 -0500

    Create basic structure in navbar.js

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <header className="navbar">
+        <h2 className="heading">📓 notebook</h2>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
