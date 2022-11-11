ef14cbe6750fa2028a787b5e154b0bad7f22b21b
Tatyana Celovsky <33358502+tcelovsky@users.noreply.github.com>
2022-11-11 10:44:57 -0500

    Add form fields to AddNote

--- src/components/AddNote/AddNote.js
+++ src/components/AddNote/AddNote.js
@@ -1,6 +1,30 @@
 import React from "react";
 import "./AddNote.css";
 
-const AddNote = () => {};
+const AddNote = () => {
+  return (
+    <div className="addnote">
+      <h1>add a new note</h1>
+      <div className="form-group">
+        <input
+          type="text"
+          className="addnote-header"
+          name="addnote-header"
+          placeholder="Note Title"
+        ></input>
+      </div>
+      <div className="form-group">
+        <textarea
+          name="addnote-description"
+          className="addnote-description"
+          placeholder="Note Text"
+        ></textarea>
+      </div>
+      <div className="addnote-button">
+        <button>add note</button>
+      </div>
+    </div>
+  );
+};
 
 export default AddNote;
