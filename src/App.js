7947e2c3154405c6b27739a3208201bbacb0d72f
Tatyana Celovsky <33358502+tcelovsky@users.noreply.github.com>
2022-11-11 10:49:12 -0500

    Import AddNote into App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { db } from "./firebase.js";
 import firebase from "firebase/app";
 import Navbar from "./components/Navbar/Navbar.js";
+import AddNote from "./components/AddNote/AddNote.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <Navbar />
+      <div className="note-section">
+        <AddNote />
+      </div>
     </div>
   );
 }
