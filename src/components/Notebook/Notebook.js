e728e8c91912edf13775871e68b2b7869e0b2bf8
Tatyana Celovsky <33358502+tcelovsky@users.noreply.github.com>
2022-11-29 12:02:15 -0500

    Start building out Notebook component

---
+++ src/components/Notebook/Notebook.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import "./Notebook.css";
+
+const Notebook = () => {
+  const deleteNotebook = (id) => {
+    firebase.database().ref("notebook").child(id).remove();
+  };
+
+  return (
+    <section className="notebook-container">
+      <div className="notebook">
+        {props.notebook.map((note, index) => (
+          <React.Fragment key={index}>
+            <div className="notebook-info" key={note.id}>
+              <div className="notebook-title">
+                <h3>{note.title}</h3>
+              </div>
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Notebook;
